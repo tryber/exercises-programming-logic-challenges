70937b9a46a2c0e3c09f737b9be0d5dbe467e813
ronaldazevedolima <ronaldazevedolima@gmail.com>
2021-08-04 17:05:23 -0300

    refatorando gabaritos

--- Arrays/Gabaritos/gabaritos.js
+++ Arrays/Gabaritos/gabaritos.js
@@ -1,6 +1,4 @@
 // 01- Maior número :
-// Dada uma lista de números inteiros, escreva um algoritmo que retorne o MAIOR número da lista.
-
 const maxNumbers = [3, 1, 8, 34, 13, 2, 5];
 
 let maxNumber = maxNumbers[0];
@@ -13,8 +11,6 @@ for (let index = 0; index < maxNumbers.length; index += 1) {
 
 
 //02 - Menor número
-// Dada uma lista de números inteiros, escreva um algoritmo que retorne o MENOR número da lista.
-
 const minNumbers = [3, 1, 8, 34, 13, -5, 2, 5];
 
 let minNumber = minNumbers[0];
@@ -26,10 +22,6 @@ for (let index = 0; index < minNumbers.length; index += 1) {
 }
 
 // 03 - Descubra se o nome esta em um array
-// Crie um algoritmo que verifica se o nome da Mica está dentro do array de pessoas facilitadoras da Trybe. 
-// let facilitators  = [Silvia, Isa, Marina, Mica, Amanda, Karine]
-// Seu algoritmo deve exibir a mensagem "O nome da Mica está no array." Caso contrário, exiba "Não faz parte". 
-
 let facilitators = ['Silvia', 'Isa', 'Marina', 'Mica', 'Amanda', 'Karine'];
 let foundMica = false;
 for(let index = 0; index < facilitators.length; index += 1) {
@@ -45,10 +37,8 @@ else {
   console.log('Não faz parte.')
 }
 
-// 04 - Todos do mesmo tipo?
-// Elabore uma função que verifica se todos os elementos de um array são do mesmo tipo.
-// A função deve retornar true ou false.
 
+// 04 - Todos do mesmo tipo?
 function verifyAllElements(array) {
   const typeOfFirst = typeof array[0];
   for (let elements of array) {
@@ -59,11 +49,8 @@ function verifyAllElements(array) {
   return true;
 }
 
-// 05 - Idade do filho mais velho
-// Pedro é pai de 4 filhos que têm idades diferentes. Esse ano ele notou que a soma da idade de seus filhos é igual à idade dele. 
-// Dada a idade de Pedro e a idade de três de seus filhos, crie um programa para computar e imprimir a idade do filho mais velho de Pedro.
-// A saída deve ser da seguinte forma: 'O filho mais velho tem X anos'
 
+// 05 - Idade do filho mais velho
 function oldestSon(pedroAge, kidsAges) {
   let sumkidsAges = 0;
 
--- Desafiadores (Juntando ferramentas)/Gabaritos/gabaritos11-16.js
+++ Desafiadores (Juntando ferramentas)/Gabaritos/gabaritos11-16.js
@@ -1,31 +1,4 @@
 // 11 - Array de notas musicais - 3 
-// Uma música normalmente é uma sequência de notas.
-// Dada a escala maior natural de Dó, cada uma das 7 notas possui um grau, representado por um algorismo romano.
-// Dó Ré  Mi  Fá Sol Lá Si
-// I  II  III IV  V  VI VII
-// Além disso, cada nota forma um acorde chamado tríade. A tríade é composta por uma sequência a partir dela com a "primeira" nota, a "terça" e a "quinta".
-// Exemplos
-// Tríade de Dó: Dó (a primeira), Mi (a terça), Sol (a quinta).
-// Tríade de Ré: Ré (a primeira), Fá (a terça), Lá (a quinta).
-// Escreva um código que recebe um array de notas, e retorna array de objetos, com os graus das notas na escala de dó e as notas que formam sua tríade no seguinte formato:
-// Entrada: ['Ré', 'Sol', 'Dó'] 
-// Saída:
-// [
-//   {
-//     degree: 'II',
-//     triad: ['Ré', 'Fá', 'Lá']
-//   },
-//   {
-//     degree: 'V',
-//     triad: [ 'Sol', 'Si', 'Ré' ]
-//   },
-//   {
-//     degree: 'I',
-//     triad: ['Dó', 'Mi', 'Sol']
-//   },
-// ]
-
-
 let musicNotes = ['Ré', 'Sol', 'Dó'];
 
 let allDegrees = {
@@ -63,14 +36,6 @@ console.log(musicNotesDetails)
 
 
 // 12 - Fatorial
-// A partir de um valor N, calcule e retorne o fatorial de N.
-// Fatorial de N = N * fatorial(N-1).
-// Definição: Fatorial de 0 = 1
-// fat(3) = 3 * fat(2) // 6
-// fat(2) = 2 * fat(1) // 2
-// fat(1) = 1 * fat(0) // 1
-// fat(0) = 1
-
 function factorial(n) {
   if (n <= 1) {
     return 1;
@@ -80,15 +45,6 @@ function factorial(n) {
 
 
 // 13 - Fibonnaci
-// A sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como sequência de Fibonacci. Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores.
-// Escreva um algoritmo que a partir de um número inteiro N mostre os N primeiros números dessa série.
-// Definição:
-//   fib(0) = 0
-//   fib(1) = 1
-//   fib(n) = fib(n-1) + fib(n-2)
-// Valor     : Saída esperada
-// n = 5     : 0 1 1 2 3
-
 function fibonnaci(n) {
   const fib = [0, 1];
   for (let count = 2; count <= n; count += 1) {
@@ -99,9 +55,8 @@ function fibonnaci(n) {
   return fib[n];
 }
 
-// 14 - Soma de números primos
-// Faça um algoritmo que some números primos de 0 à 150.
 
+// 14 - Soma de números primos
 function isPrimo(numero) {
   if(numero <= 1) 
       return false;
@@ -123,8 +78,6 @@ for(let index = 0; index <= 150; index += 1) {
 
 
 // 15 - Fatorial e média
-// Faça uma função que calcule o fatorial de um número e em seguida, faça a média dos fatoriais de 0 a 5.
-
 function fatorial(numero) {
   if(numero === 0 || numero === 1) {
       return 1;
@@ -143,31 +96,8 @@ for(let index = 0; index <= 5; index += 1) {
   som += fatorial(index);
 }
 
-// 16 - Validação de CPF
-// Desenvolva uma função `cpfValidator`, que recebe uma string no formato de CPF `xxx.xxx.xxx-xx` e verifica se o CPF passado por parâmetro é válido ou não.
-// Considere um CPF com os seguintes dígitos:
-// `abc.def.ghi-jk`
-// Para descobrirmos o primeiro dígito verificado `j`, procedemos da seguinte maneira: multiplicamos o primeiro por 1, o segundo por 2, o terceiro por 3, o quarto por 4 e vamos assim até multiplicarmos o nono por 9. Então, somamos tudo isto. O dígito `j` é o resto da divisão da soma por 11 (ou 0, caso o resto seja 10).
-// Para o segundo dígito verificador `k`, temos o seguinte: multiplicamos o segundo digito por 1, o terceiro por 2, o quarto por 3 e vamos assim até multiplicarmos o décimo (primeiro dígito verificador) por 9. Então, somamos tudo isto. O dígito `k` será o resto da divisão da soma por 11 (ou 0, caso o resto seja 10).
-// Sabendo que isso vale para 100% dos CPFs, sua missão é implementar uma função que, dado um CPF, diga se ele é válido ou não.
-// A saída da sua função deve ser uma string. Quando o CPF for válido, o retorno deve ser `'CPF válido'`; quando o CPF for inválido, o retorno deve ser `'CPF inválido'`
-// **Importante:**
-// Uma sequência de números iguais, como por exemplo `'000.000.000-00'`, também é um CPF inválido 😉
-// Exemplo:
-// ```
-// // Entrada:
-// '397-606-448-34'
-// // Saída:
-// 'CPF inválido'
-// // Entrada:
-// '166.354.840-43'
-// // Saída:
-// 'CPF válido'
-// ```
-// **O que será verificado:**
-// - A função deve retornar a mensagem `'CPF inválido'`, quando o CPF for inválido
-// - A função deve retornar a mensagem `'CPF válido'`, quando o CPF for válido.
 
+// 16 - Validação de CPF
 function calcCpfFirstDigit(cpfArray) {
   const sum = cpfArray.slice(0, 9).reduce((sum, number, index) => sum + number * (index + 1), 0);
   const rest = sum % 11;
