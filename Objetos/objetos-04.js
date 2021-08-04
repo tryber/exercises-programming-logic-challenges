5df019291c0401e4291d16dc43963c2eccbd1f69
ronaldazevedolima <ronaldazevedolima@gmail.com>
2021-08-04 18:12:54 -0300

    ex objetos

---
+++ Objetos/objetos-04.js
@@ -0,0 +1,25 @@
+// 04 - Quem é a pessoa mais velha?
+//Dado um array de pessoas, identifique qual tem a maior idade e retorne o nome da pessoa e sua idade.
+//A saída deve ser da seguinte forma: 'X tem a maior idade, com X anos'
+const people = [
+  { name: 'Ramon', age: 35 },
+  { name: 'José', age: 28 },
+  { name: 'Amanda', age: 19 },
+  { name: 'Tamires', age: 22 },
+  { name: 'Xablau', age: 42 },
+  { name: 'Lucas', age: 37 },
+  { name: 'Oswaldo', age: 13 },
+  { name: 'Enzo', age: 13 },
+  { name: 'Marilia', age: 21 },
+  { name: 'Gabriela', age: 25 },
+  { name: 'Silvia', age: 43 },
+  { name: 'Gabriel', age: 25 },
+  { name: 'Ricardo', age: 51 },
+  { name: 'Joaquim', age: 9 },
+  { name: 'Ronald', age: 18 },
+  { name: 'Paulo', age: 27 },
+  { name: 'Emilia', age: 45 },
+];
+
+
+// Desenvolva seu código aqui
