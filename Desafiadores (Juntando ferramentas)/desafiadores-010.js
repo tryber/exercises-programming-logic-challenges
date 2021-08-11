e16230773030bd1c2f9b01faf17cf20f11cef670
Ronald de Azevedo Lima <ronaldazevedolima@gmail.com>
2021-08-11 13:35:21 -0300

    Update desafiadores-010.js

--- Desafiadores (Juntando ferramentas)/desafiadores-010.js
+++ Desafiadores (Juntando ferramentas)/desafiadores-010.js
@@ -1,4 +1,4 @@
-// 10 - Nega Numeros
+// 10 - Nega Números
 // Elabore uma função que ao receber um array de números, retorne uma cópia do array com todos os
 // números negados. Exemplo: negate([2,3,4]) => [-2,-3-,4];   negate([-2,-1,3]) => [2,1,-3];
 
