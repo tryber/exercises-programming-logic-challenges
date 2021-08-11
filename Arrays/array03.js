67e0e1b7f9aa6fd70dc5a1a5b12418c65a00d657
Ronald de Azevedo Lima <ronaldazevedolima@gmail.com>
2021-08-11 13:30:32 -0300

    Update array03.js

--- Arrays/array03.js
+++ Arrays/array03.js
@@ -1,4 +1,4 @@
-// 03 - Descubra se o nome esta em um array
+// 03 - Descubra se o nome está em um array
 // Crie um algoritmo que verifica se o nome da Mica está dentro do array de pessoas facilitadoras da Trybe. 
 // let facilitators  = [Silvia, Isa, Marina, Mica, Amanda, Karine]
 // Seu algoritmo deve exibir a mensagem "O nome da Mica está no array." Caso contrário, exiba "Não faz parte". 
