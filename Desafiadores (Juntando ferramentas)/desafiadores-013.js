d85cc1c8f2b4572cacc15f68f58c6c7d1ebb600f
Ronald de Azevedo Lima <ronaldazevedolima@gmail.com>
2021-08-11 13:35:56 -0300

    Update desafiadores-013.js

--- Desafiadores (Juntando ferramentas)/desafiadores-013.js
+++ Desafiadores (Juntando ferramentas)/desafiadores-013.js
@@ -1,4 +1,4 @@
-// 13 - Fibonnaci
+// 13 - Fibonacci
 // A sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como sequência de Fibonacci. Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores.
 // Escreva um algoritmo que a partir de um número inteiro N mostre os N primeiros números dessa série.
 // Definição:
