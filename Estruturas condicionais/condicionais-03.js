e70749070f7fd9ba445a71e26b556ac27e98fb3e
Ronald de Azevedo Lima <ronaldazevedolima@gmail.com>
2021-08-12 08:25:51 -0300

    Update condicionais-03.js

--- Estruturas condicionais/condicionais-03.js
+++ Estruturas condicionais/condicionais-03.js
@@ -1,12 +1,12 @@
 // 03 - Mostre a classe do aventureiro
-//A Trybe tem um grupo de aventureiros. Quando o nome de algum é invocado, a classe a qual essa pessoa faz parte é apresentada.
-//É sua função criar um programa que recebe o nome de um aventureiro e, de acordo com este nome, revela uma das seguintes classes:
-//'Tunico' - 'Mestre dos Magos'
-//'Cremoso' - 'Cavaleiro Negro'
-//'Gordinho' - 'Pirata'
-//'Lobo Solitário' - 'Samurai'
-//'Ousado' - 'Mago'
-//'Anjo Loiro' - 'Clérigo'
-//Caso o nome informado não faça parte do esquadrão , exiba a mensagem: Não foi possível encontrar um aventureiro com esse nome.  
+// A Trybe tem um grupo de aventureiros. Quando o nome de algum é invocado, a classe a qual essa pessoa faz parte é apresentada.
+// É sua função criar um programa que recebe o nome de um aventureiro e, de acordo com este nome, revela uma das seguintes classes:
+// 'Tunico' - 'Mestre dos Magos'
+// 'Cremoso' - 'Cavaleiro Negro'
+// 'Gordinho' - 'Pirata'
+// 'Lobo Solitário' - 'Samurai'
+// 'Ousado' - 'Mago'
+// 'Anjo Loiro' - 'Clérigo'
+// Caso o nome informado não faça parte do esquadrão, exiba a mensagem: Não foi possível encontrar um aventureiro com esse nome.  
 
 // Desenvolva seu código aqui
