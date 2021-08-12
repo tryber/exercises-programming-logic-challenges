6f156ea474cce247d8bbf267397a619186ece5ed
Ronald de Azevedo Lima <ronaldazevedolima@gmail.com>
2021-08-12 08:24:25 -0300

    Update condicionais-012.js

--- Estruturas condicionais/condicionais-012.js
+++ Estruturas condicionais/condicionais-012.js
@@ -1,6 +1,6 @@
 // 12 - IMC
-//A classificação do Índice de Massa Corporal (IMC), pode ajudar a identificar obesidade ou desnutrição em crianças, adolescentes, adultos e idosos.
-// O IMC é a relação entre peso e altura e o cálculo é feito de acordo com a fórmula: IMC = peso/ (altura x altura), devendo o peso estar em kg e a altura em metro, e o resultado é dado em kg/m2. Depois de obter o resultado, é verificado em que faixa o resultado se encontra, podendo indicar:
+// A classificação do Índice de Massa Corporal (IMC), pode ajudar a identificar obesidade ou desnutrição em crianças, adolescentes, adultos e idosos.
+// O IMC é a relação entre peso e altura e o cálculo é feito de acordo com a fórmula: IMC = peso/ (altura x altura), onde o peso é dado em Kg (quilogramas), a altura em m (metros) e o IMC em kg/m2 (quilograma por metro quadrado). Depois de obter o resultado, é verificado em que faixa o resultado se encontra, podendo indicar:
 
 // Magreza, quando o resultado é menor que 18,5 kg/m2;
 // Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
