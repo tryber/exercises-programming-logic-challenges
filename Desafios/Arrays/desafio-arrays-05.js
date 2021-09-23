2f86465e4243d12d07d1d99a67baf1af78bf8842
Daniel <marcio.daniel@msn.com>
2021-09-23 14:55:10 -0300

    Alteracoes no Desafio de Arrays

--- Desafios/Arrays/desafio-arrays-05.js
+++ Desafios/Arrays/desafio-arrays-05.js
@@ -2,7 +2,7 @@
 
 05 - Encontre o filho mais velho
 
-Dada a idade de Pedro e um array com a idade de três de seus filhos, escreva um algoritmo que irá receber esses dados para computar e retornar a idade do filho mais velho de Pedro.
+Dada a idade de Pedro e um array com as idades de três de seus filhos, escreva um algoritmo que irá receber esses dados para computar e retornar a idade do filho mais velho de Pedro.
 
 A saída deve ter o seguinte formato: 'O filho mais velho tem "X" anos.'.
 
