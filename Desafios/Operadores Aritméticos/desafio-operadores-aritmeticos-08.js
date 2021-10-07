929307576e1ad3c3167a504ae67193140392947f
NandaInstrucao <84472298+NandaInstrucao@users.noreply.github.com>
2021-10-07 09:27:58 -0300

    Update Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-08.js

--- Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-08.js
+++ Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-08.js
@@ -2,7 +2,7 @@
 
 08 - Conta do restaurante
 
-Escreva um algoritmo que recebe o valor de um prato, calcule e retorne valor final acrescendo 10% de taxa por ser uma área turística e mais 5% de corjeta para o garçon.
+Escreva um algoritmo que recebe o valor de um prato, calcule e retorne o valor final dele acrescendo 10% de taxa por ser uma área turística e mais 5% de gorjeta para o garçon.
 
 Obs: Retornar um valor do tipo number com duas casa decimais.
 
