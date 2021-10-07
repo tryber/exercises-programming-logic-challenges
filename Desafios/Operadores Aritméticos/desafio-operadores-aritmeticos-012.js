3945be4c3d2d2ccf4e67f447f5cd9d2f6bb10023
NandaInstrucao <84472298+NandaInstrucao@users.noreply.github.com>
2021-10-07 09:29:21 -0300

    Update Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-012.js

--- Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-012.js
+++ Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-012.js
@@ -2,7 +2,7 @@
 
 12 - Peso e preço do prato
 
-Um restaurante comprou uma balança nova e precisa adicionar um programa que calcula o valor que a pessoa vai pagar.
+Um restaurante comprou uma balança nova e precisa adicionar um programa que calcula o valor que a pessoa vai pagar pelo prato de comida.
 
 Escreva um algoritmo que recebe o valor por kilo e o peso do prato e
 calcule o valor da refeição, retornando uma mensagem no seguinte formato:
