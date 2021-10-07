12fa3ac1cf7bc26dd6f86c8880828b95f1b374ab
NandaInstrucao <84472298+NandaInstrucao@users.noreply.github.com>
2021-10-07 09:28:41 -0300

    Update Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-02.js

--- Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-02.js
+++ Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-02.js
@@ -5,7 +5,7 @@
 Na sapataria do Faraó todas as pessoas da área de vendas recebem um salário fixo acrescido de 4% de comissão sobre as vendas. 
 
 Escreva um algoritmo que receba o salário fixo de vendedores da sapataria do Faraó
-e o valor de suas vendas, e assim calcule e retorne a comissão e seu salário final no formato:
+e o valor de suas vendas,  calcule e retorne a comissão e seu salário final no formato:
 
 ---------------------------------------
 "A comissão é: x e o ganho total é: y".
