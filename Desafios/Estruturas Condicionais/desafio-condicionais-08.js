9821f27b2ca717d8bbe90df83466a2136a3f00a7
Márcio Daniel <marcio.daniel@msn.com>
2022-04-07 11:59:43 -0300

    Corrigindo Enunciado
    Pessoas estudantes ainda não viram função nesse dia do bloco.

--- Desafios/Estruturas Condicionais/desafio-condicionais-08.js
+++ Desafios/Estruturas Condicionais/desafio-condicionais-08.js
@@ -1,16 +1,8 @@
 /*
-
 08 - Descubra a idade mínima
 
 Escreva um algoritmo que recebe a idade de Marina, Silvia e Iza e retorne a mensagem informando a pessoa mais jovem no seguinte formato:
 ---------------------------------------------------
 "x é a pessoa mais jovem e possui y anos de idade."
 ---------------------------------------------------
-
 */
-
-function findYoungerPerson(marinaAge, silviaAge, izaAge){
-  // Desenvolva seu código nessa função
-}
-
-module.exports = findYoungerPerson;
