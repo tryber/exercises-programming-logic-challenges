a11f756e191bd50767c685191d41fc526bb8b5c5
Márcio Daniel <marcio.daniel@msn.com>
2022-04-07 14:22:06 -0300

    Revert "Update desafio-condicionais-01.js"
    This reverts commit d70afa1c9de8f256b572a9e6cc1591f028c2f214.

--- Desafios/Estruturas Condicionais/desafio-condicionais-01.js
+++ Desafios/Estruturas Condicionais/desafio-condicionais-01.js
@@ -1,4 +1,7 @@
 /*
+
+01 - Lanchonete da Trybe
+
 A Trybe abriu uma lanchonete e precisa criar um menu de opções para que as pessoas consigam escolher qual lanche maravilhoso irão pedir.
 
 Para isso escreva um algoritmo que recebe o número da opção escolhida conforme tabela abaixo e retorne a string inteira do respectivo lanche:
@@ -14,4 +17,12 @@ Caso a pessoa escolha uma opção que não existe na tabela acima, retorne:
 "Não temos esta opção ainda :("
 -------------------------------
 
+Utilize Switch/Case para resolver este problema.
 
+*/
+
+function optionsMenu(number){
+  // Desenvolva seu código nessa função
+}
+
+module.exports = optionsMenu;
