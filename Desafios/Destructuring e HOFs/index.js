5e145b5bb501e905978a195099ef1ff60e758421
Carol Silva <contatocarolinebs@gmail.com>
2022-04-22 17:06:53 -0300

    Criado index para pasta

---
+++ Desafios/Destructuring e HOFs/index.js
@@ -0,0 +1,15 @@
+const getBandsInformation = require('./desafio-hofs-01.js');
+const getBestAlbuns = require('./desafio-hofs-02.js');
+const getBandsName = require('./desafio-hofs-03.js');
+const usaFilter = require('./desafio-hofs-04.js');
+const rockFilter = require('./desafio-hofs-05.js');
+const highestRatingFilter = require('./desafio-hofs-06.js');
+
+module.exports = {
+  getBandsInformation,
+  getBestAlbuns,
+  getBandsName,
+  usaFilter,
+  rockFilter,
+  highestRatingFilter
+};
