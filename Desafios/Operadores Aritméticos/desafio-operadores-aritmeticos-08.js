3faeeba777a2aa05396a65b422ed20f279848692
Kissyla <67333783+mottak@users.noreply.github.com>
2022-05-30 15:54:41 -0300

    Update desafio-operadores-aritmeticos-08.js
    modificacao enunciado exercicio 8 Operadores Aritmeticos

--- Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-08.js
+++ Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-08.js
@@ -5,6 +5,7 @@
 Escreva um algoritmo que recebe o valor de um prato, calcule e retorne o valor final dele acrescendo 10% de taxa por ser uma área turística e mais 5% de gorjeta para o garçon.
 
 Obs: Retornar um valor do tipo number com duas casa decimais.
+Obs2: Dependendo do método usado para arredondar o tipo da sua variavél pode mudar, e aqui o retorno DEVE ser do tipo NUMBER. Talvez seja preciso converter para number ao final.
 
 */
 
