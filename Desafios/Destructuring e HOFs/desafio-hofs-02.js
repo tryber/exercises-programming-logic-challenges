a774e1eedc5b84519b0147609b25222d44705af7
Renan Fernandes <renan.fernandes@betrybe.com>
2022-06-29 14:24:08 -0300

    mentoria de revisão turma 23

--- Desafios/Destructuring e HOFs/desafio-hofs-02.js
+++ Desafios/Destructuring e HOFs/desafio-hofs-02.js
@@ -4,9 +4,30 @@ const data = require('./data');
 retornando uma string no seguinte formato: "os melhores álbuns do Radiohead: In Rainbows,Kid A,OK Computer,Pablo Honey"
 */
 
-const getBestAlbuns = () => {
+const getBestAlbuns = (bands) => {
+    const [ primeiraBanda ] = bands;
+    const { bandName, bestAlbuns } = primeiraBanda;
 
+    const albumNames = bestAlbuns.map((bestAlbum) => {
+        return bestAlbum.name;
+    });
+
+    const stringAlbumNames = albumNames.join(',')
+
+    return `os melhores álbuns do ${bandName}: ${stringAlbumNames}`;
 }
+
+// const getBestAlbuns = ([ primeiraBanda ]) => {
+//     const { bandName, bestAlbuns } = primeiraBanda;
+
+//     const albumNames = bestAlbuns.map(({ name }) => name);
+
+//     const stringAlbumNames = albumNames.join(',')
+
+//     return `os melhores álbuns do ${bandName}: ${stringAlbumNames}`;
+// }
+
+console.log(getBestAlbuns(data.bands));
 
 
 module.exports = getBestAlbuns;
