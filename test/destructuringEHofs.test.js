ef55198344a1640c5168478bf1391836555b840e
Carlos Eduardo <cpwaldow@gmail.com>
2022-09-12 16:54:09 -0300

    add correctly object values

--- test/destructuringEHofs.test.js
+++ test/destructuringEHofs.test.js
@@ -16,13 +16,47 @@ describe("Desafios de Destructuring e HOFs.", () => {
       const expectedArray = [
         {
           bandName: 'Radiohead',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
+          bestAlbuns: [
+            {
+              name: "In Rainbows",
+              rate: 100
+            },
+            {
+              name: "Kid A",
+              rate: 95
+            },
+            {
+              name: "OK Computer",
+              rate: 90
+            },
+            {
+              name: "Pablo Honey",
+              rate: 80
+            }
+          ],
           genre: 'Rock',
           country: 'England'
         },
         {
           bandName: 'Avenged Sevenfold',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
+          bestAlbuns: [
+            {
+              name: "City of Evil",
+              rate: 99
+            },
+            {
+              name: "Avenged Sevenfold",
+              rate: 98
+            },
+            {
+              name: "Sounding the Seventh Trumpet",
+              rate: 95
+            },
+            {
+              name: "Diamonds in the Rough",
+              rate: 80
+            }
+          ],
           genre: 'Hard Rock',
           country: 'United States'
         }
