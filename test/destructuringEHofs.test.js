94365b6e0ff0f62ceece269142c65ffb5c849e65
Carlos Eduardo <cpwaldow@gmail.com>
2022-09-19 17:44:00 -0300

    feat: update challenge 05 functions, test and rubric

--- test/destructuringEHofs.test.js
+++ test/destructuringEHofs.test.js
@@ -113,149 +113,17 @@ describe("Desafios de Destructuring e HOFs.", () => {
     it("O retorno da função deve ser um array", () => {
       expect(Array.isArray(rockFilter())).toBeTruthy();
     });
-/*     it("O retorno da função deve ser o array esperado", () => {
-      const expectedArray2 = [
-        {
-          bandName: 'Radiohead',
-          bestAlbuns: [
-            {
-              name: "In Rainbows",
-              rate: 100
-            },
-            {
-              name: "Kid A",
-              rate: 95
-            },
-            {
-              name: "OK Computer",
-              rate: 90
-            },
-            {
-              name: "Pablo Honey",
-              rate: 80
-            }
-          ],
-          genre: 'Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Avenged Sevenfold',
-          bestAlbuns: [
-            {
-              name: "City of Evil",
-              rate: 99
-            },
-            {
-              name: "Avenged Sevenfold",
-              rate: 98
-            },
-            {
-              name: "Sounding the Seventh Trumpet",
-              rate: 95
-            },
-            {
-              name: "Diamonds in the Rough",
-              rate: 80
-            }
-          ],
-          genre: 'Hard Rock',
-          country: 'United States'
-        },
-        {
-          bandName: 'Arctic Monkeys',
-          bestAlbuns: [
-            {
-              name: "Humbug",
-              rate: 100
-            },
-            {
-              name: "AM",
-              rate: 99
-            },
-            {
-              name: "Favourite Worst Nightmare",
-              rate: 95
-            },
-            {
-              name: "Whatever People Say I Am, That's What I'm Not",
-              rate: 90
-            }
-          ],
-          genre: 'Indie Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Pink Floyd',
-          bestAlbuns: [
-            {
-              name: "Wish You Were Here",
-              rate: 100
-            },
-            {
-              name: "Dark Side of the Moon",
-              rate: 100
-            },
-            {
-              name: "Animals",
-              rate: 100
-            },
-            {
-              name: "A Saucerful of Secrets",
-              rate: 98
-            }
-          ],
-          genre: 'Progressive Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Foo Fighters',
-          bestAlbuns: [
-            {
-              name: "Wasting Light",
-              rate: 100
-            },
-            {
-              name: "Sonic Highways",
-              rate: 90
-            },
-            {
-              name: "Concrete and Gold",
-              rate: 90
-            },
-            {
-              name: "Echoes, Silence, Patience & Grace",
-              rate: 85
-            }
-          ],
-          genre: 'Rock',
-          country: 'United States'
-        },
-        {
-          bandName: 'Red Hot Chili Peppers',
-          bestAlbuns: [
-            {
-              name: "Stadium Arcadium",
-              rate: 100
-            },
-            {
-              name: "Californication",
-              rate: 99
-            },
-            {
-              name: "By the Way",
-              rate: 97
-            },
-            {
-              name: "The Getaway",
-              rate: 95
-            }
-          ],
-          genre: 'Rock',
-          country: 'United States'
-        }
+    it("O retorno da função deve ser o array com o nome de todas as  bandas", () => {
+      const expectedArray = [
+        'Radiohead',
+        'Avenged Sevenfold',
+        'Arctic Monkeys',
+        'Pink Floyd',
+        'Foo Fighters',
+        'Red Hot Chili Peppers'
       ]
-      expect(rockFilter()).toMatchObject(expectedArray2);
-    }); */
+      expect(rockFilter()).toEqual(expectedArray);
+    });
   });
 
   describe("06 - Testando a Função highestRatingFilter.", () => {
