e7200269ef54a3b3dd069c06d957bd391e7a7e20
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-12-20 11:18:38 -0300

    Operadores aritiméticos: exercício 06 - teste corrigido e exercício resolvido

--- Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-06.js
+++ Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-06.js
@@ -5,7 +5,7 @@
 André resolveu criar seu próprio programa de cálculo de Índice de Massa Corporal (IMC).
 
 Considere  que:
-* A fórmula para calcular o IMC é divisão do peso (em kg) pela altura ao quadrado (em metros);
+* A fórmula para calcular o IMC é divisão da altura ao quadrado (em metros) pelo peso (em kg);
 * André pesa 75 kg e sua altura é 1,80 m;
 
 Escreva o algoritmo usado por André para calcular seu próprio IMC e o retorne.
@@ -13,7 +13,7 @@ Escreva o algoritmo usado por André para calcular seu próprio IMC e o retorne.
 */
 
 function IMC(weight, height){
-  // Desenvolva seu código nessa função
+  return weight / (height * height);
 }
 
 module.exports = IMC;
--- test/operadoresAritmeticos.test.js
+++ test/operadoresAritmeticos.test.js
@@ -116,9 +116,9 @@ describe("Desafios de Operadores Aritméticos.", () => {
   describe(" 06 - Testando a Função IMC.", () => {
     const height = +`1.${getRandomInt(40, 99)}`
     const weight = getRandomInt(40, 120);
-    const imc =  (height * height) / weight;
-    it("Ao passar os argumentos 70 e 1.8 , deve retornar: 0.04628571428571429", () => {
-      expect(IMC(70, 1.8)).toBe(0.04628571428571429);
+    const imc =  weight / (height * height);
+    it("Ao passar os argumentos 70 e 1.8 , deve retornar: 21.604938271604937", () => {
+      expect(IMC(70, 1.8)).toBe(21.604938271604937);
     });
     it(`Ao passar os argumentos ${weight} e ${height}, deve retornar: ${imc}`, () => {
       expect(IMC(weight, height)).toBe(imc);
